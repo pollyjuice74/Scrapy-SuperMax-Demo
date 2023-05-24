6dcf5bb5433200ba591641d5d71e15aa3ed6182c
pollyjuice74 <hernandez.aht82836@gmail.com>
2023-05-24 09:50:21 -0400

    Recursive section crawl

--- SuperMax/SuperMax/spiders/supermax_spider.py
+++ SuperMax/SuperMax/spiders/supermax_spider.py
@@ -6,6 +6,9 @@ In scrapy shell:
     4) name = products.css('h3 a::text').get()
     5) price = products.css('p.text-center.precio::text').get().replace('$', '').strip()
     6) link = products.css('h3 a::attr(href)').get()
+
+Selecting each page link: 
+    links = response.css('div#departments-menu a::attr(href)')
 """
 
 import scrapy
@@ -15,12 +18,40 @@ class SupMaxSpider(scrapy.Spider):
     start_urls = ['https://www.supermaxonline.com/shopping-home.html']
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.4567.789 Safari/537.36'}
 
-
-    def parse(self, response):
-        status_code = response.status
-        print(f"Status code: {status_code}")
-
     def start_requests(self):
         for url in self.start_urls:
-            yield scrapy.Request(url, headers=headers)
-        
+            yield scrapy.Request(url, headers=self.headers)
+
+    def parse(self, response):
+        # Extract the paths from the response
+        paths = response.css('div#departments-menu a::attr(href)').getall()
+        visited_urls = set()
+
+        # Initializes recursion for each path
+        for path in paths: 
+            url = response.urljoin(path)
+            if url not in visited_urls:
+                visited_urls.add(url)
+                yield scrapy.Request(url, callback=self.parse_products, headers=self.headers) 
+
+
+    def parse_products(self, response):
+        # Check 'Load_More' button
+        load_more_button = response.css('button.load-more-button')
+
+        if load_more_button:
+            yield scrapy.FormRequest.from_response(
+                response,
+                clickdata = scrapy.FormRequest.ClickData(class_='btn', text='Ver más'),
+                callback=self.parse_products, # Recursively call while there is a 'Load_More' button
+                headers=self.headers
+            )
+
+        else: # Base case
+            # Scrape product info
+            for products in response.css('div.product-space'):
+                yield {
+                    "name": products.css('h3 a::text').get(),
+                    "price": products.css('p.text-center.precio::text').get().replace('$', '').strip(),
+                    "link": products.css('h3 a::attr(href)').get(),
+                }
